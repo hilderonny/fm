ce7c0756f745e641c05ae4a6d31dbf44bd1d28f1
Ronny Hildebrandt <ronny.hildebrandt@avorium.de>
2018-04-09 15:30:32 +0200

    Prepared dynamic API tests. #AVTFM-163 work Entwicklung 1h

--- test/api/dynamic.js
+++ test/api/dynamic.js
@@ -60,70 +60,295 @@ describe('API dynamic', () => {
     
     describe('GET/children/:forlist/:recordtypename/:entityname', () => {
 
-        xit('', async () => {
+        xit('responds without authentication with 403', async () => {
+        });
+
+        xit('responds without read permission with 403', async () => {
+        });
+
+        xit('responds when the logged in user\'s (normal user) client has no access to the module of the record type, with 403', async () => {
+        });
+
+        xit('responds when the logged in user\'s (administrator) client has no access to the module of the record type, with 403', async () => {
+        });
+
+        xit('responds with 400 when the recordtypename is invalid', async () => {
+        });
+
+        xit('responds with 404 when the entityname is invalid', async () => {
+        });
+
+        xit('responds with empty list when there are no children for the given list name', async () => {
+        });
+
+        xit('responds with 404 when the object to request does not belong to client of the logged in user', async () => {
+        });
+
+        xit('responds with a list of children of a given entity which are contained in the given list', async () => {
+            // datatypename, icon, haschildren
+        });
+
+        xit('responds with an empty list when the element has no children', async () => {
+        });
+
+        xit('does not return children of recordtypes where the user has no access to', async () => {
         });
 
     });
 
     describe('GET/hierarchytoelement/:forlist/:recordtypename/:entityname', () => {
 
-        xit('', async () => {
+        xit('responds without authentication with 403', async () => {
+        });
+
+        xit('responds without read permission with 403', async () => {
+        });
+
+        xit('responds when the logged in user\'s (normal user) client has no access to the module of the record type, with 403', async () => {
+        });
+
+        xit('responds when the logged in user\'s (administrator) client has no access to the module of the record type, with 403', async () => {
+        });
+
+        xit('responds with 400 when the recordtypename is invalid', async () => {
+        });
+
+        xit('responds with 404 when the entityname is invalid', async () => {
+        });
+
+        xit('responds with empty list when the element is a root element without any parents', async () => {
+        });
+
+        xit('responds with empty list when there are no parents for the given list name', async () => {
+        });
+
+        xit('responds with 404 when the object to request does not belong to client of the logged in user', async () => {
+        });
+
+        xit('responds with a list of parents and their children ordered from root (1st) to the element (last)', async () => {
+        });
+
+        xit('does not return children of the parents for recordtypes where the user has no access to', async () => {
+        });
+
+        xit('does not return parents for recordtypes where the user has no access to', async () => {
         });
 
     });
 
     describe('GET/parentpath/:forlist/:recordtypename/:entityname', () => {
 
-        xit('', async () => {
+        xit('responds without authentication with 403', async () => {
+        });
+
+        xit('responds without read permission with 403', async () => {
+        });
+
+        xit('responds when the logged in user\'s (normal user) client has no access to the module of the record type, with 403', async () => {
+        });
+
+        xit('responds when the logged in user\'s (administrator) client has no access to the module of the record type, with 403', async () => {
+        });
+
+        xit('responds with 400 when the recordtypename is invalid', async () => {
+        });
+
+        xit('responds with 404 when the entityname is invalid', async () => {
+        });
+
+        xit('responds with empty list when the element is a root element without any parents', async () => {
+        });
+
+        xit('responds with empty list when there are no parents for the given list name', async () => {
+        });
+
+        xit('responds with 404 when the object to request does not belong to client of the logged in user', async () => {
+        });
+
+        xit('responds with a list of parents ordered from root (1st) to the element (last)', async () => {
+        });
+
+        xit('does not return parents for recordtypes where the user has no access to', async () => {
         });
 
     });
 
     describe('GET/rootelements/:forlist', () => {
 
-        xit('', async () => {
+        xit('responds without authentication with 403', async () => {
+        });
+
+        xit('responds without read permission with 403', async () => {
+        });
+
+        xit('responds when the logged in user\'s (normal user) client has no access to the base module, with 403', async () => {
+        });
+
+        xit('responds when the logged in user\'s (administrator) client has no access to the base module, with 403', async () => {
+        });
+
+        xit('responds with empty list when there are no root elements for the given list name', async () => {
+        });
+
+        xit('responds with a list of root elements', async () => {
+            // check for haschildren!
+        });
+
+        xit('does not return root elements for recordtypes where the user has no access to', async () => {
         });
 
     });
 
     describe('GET/:recordtypename', () => {
 
-        xit('', async () => {
+        xit('responds without authentication with 403', async () => {
+        });
+
+        xit('responds without read permission with 403', async () => {
+        });
+
+        xit('responds when the logged in user\'s (normal user) client has no access to the module of the record type, with 403', async () => {
+        });
+
+        xit('responds when the logged in user\'s (administrator) client has no access to the module of the record type, with 403', async () => {
+        });
+
+        xit('responds with 400 when the recordtypename is invalid', async () => {
+        });
+
+        xit('responds with empty list when there are no elements of the given record type', async () => {
+        });
+
+        xit('responds with empty list when the user has no access to the record type', async () => {
+        });
+
+        xit('responds with a list of all elements of the given record type', async () => {
         });
 
     });
 
     describe('GET/:recordtypename/:entityname', () => {
 
-        xit('', async () => {
+        xit('responds without authentication with 403', async () => {
+        });
+
+        xit('responds without read permission with 403', async () => {
+        });
+
+        xit('responds when the logged in user\'s (normal user) client has no access to the module of the record type, with 403', async () => {
+        });
+
+        xit('responds when the logged in user\'s (administrator) client has no access to the module of the record type, with 403', async () => {
+        });
+
+        xit('responds with 400 when the recordtypename is invalid', async () => {
+        });
+
+        xit('responds with 404 when the entityname is invalid', async () => {
+        });
+
+        xit('responds with 404 when the object to request does not belong to client of the logged in user', async () => {
+        });
+
+        xit('responds with the record data of the given entityname', async () => {
         });
 
     });
 
     describe('POST/:recordtypename', () => {
 
-        xit('', async () => {
+        xit('responds without authentication with 403', async () => {
+        });
+
+        xit('responds without write permission with 403', async () => {
+        });
+
+        xit('responds when the logged in user\'s (normal user) client has no access to the module of the record type, with 403', async () => {
+        });
+
+        xit('responds when the logged in user\'s (administrator) client has no access to the module of the record type, with 403', async () => {
+        });
+
+        xit('responds with 400 when the recordtypename is invalid', async () => {
+        });
+
+        xit('responds with 409 when the name can be defined but is used by another entity', async () => {
+        });
+
+        xit('creates a new entity and generates a name when name cannot be defined', async () => {
+        });
+
+        xit('creates a new entity and generates a name when name can be defined but was not sent', async () => {
+        });
+
+        xit('recalculates the referenced parents when the object to create is a relation of type "parent-child"', async () => {
+        });
+
+        xit('returns the name of the created entity', async () => {
+        });
+
+        xit('recalculates the formulas of the created entity immediately', async () => {
         });
 
     });
 
-    describe('POST/:recordtypename/:entityname', () => {
+    describe('PUT/:recordtypename/:entityname', () => {
 
-        xit('', async () => {
+        xit('responds without authentication with 403', async () => {
         });
 
-    });
-
-    describe('GET/', () => {
-
-        xit('', async () => {
+        xit('responds without write permission with 403', async () => {
         });
 
-    });
+        xit('responds when the logged in user\'s (normal user) client has no access to the module of the record type, with 403', async () => {
+        });
 
-    describe('GET/', () => {
+        xit('responds when the logged in user\'s (administrator) client has no access to the module of the record type, with 403', async () => {
+        });
 
-        xit('', async () => {
+        xit('responds with 400 when the recordtypename is invalid', async () => {
+        });
+
+        xit('responds with 404 when the entityname is invalid', async () => {
+        });
+
+        xit('responds with 404 when the object to update does not belong to client of the logged in user', async () => {
+        });
+
+        xit('does not update the name when it was sent', async () => {
+        });
+
+        xit('updates all given fields of the entity and returns 200', async () => {
+        });
+
+        xit('does not update fields which are not sent', async () => {
+        });
+
+        xit('recalculates the formerly referenced parents when the object to update is a relation and its type changed away from "parent-child"', async () => {
+        });
+
+        xit('recalculates the formerly referenced parents when the object to update is a relation and its type changed to "parent-child"', async () => {
+        });
+
+        xit('recalculates the formerly referenced parents when the object to update is a relation and its first relation element changed', async () => {
+        });
+
+        xit('recalculates the formerly referenced parents when the object to update is a relation and its second relation element changed', async () => {
+        });
+
+        xit('recalculates the new referenced parents when the object to update is a relation and its type changed away from "parent-child"', async () => {
+        });
+
+        xit('recalculates the new referenced parents when the object to update is a relation and its type changed to "parent-child"', async () => {
+        });
+
+        xit('recalculates the new referenced parents when the object to update is a relation and its first relation element changed', async () => {
+        });
+
+        xit('recalculates the new referenced parents when the object to update is a relation and its second relation element changed', async () => {
+        });
+
+        xit('recalculates the formulas of the updated entity immediately', async () => {
         });
 
     });
