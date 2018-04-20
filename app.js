07f71cef75cf06347317fedfa46ee1e3420971c4
ranafawzi <ranafawzi85@yahoo.com>
2018-04-20 13:44:42 +0200

    server installation test #AVTFM-176 work 7h

--- app.js
+++ app.js
@@ -193,6 +193,53 @@ async function init() {
         module.exports.server = server;
     }
 
+    const webdav = require('webdav-server').v2;
+
+    // User manager (tells who are the users)
+    const userManager = new webdav.SimpleUserManager();
+    const user = userManager.addUser('rf', 'rf', false);
+
+    // Privilege manager (tells which users can access which files/folders)
+    const privilegeManager = new webdav.SimplePathPrivilegeManager();
+    privilegeManager.setRights(user, '/documents', [ 'all' ]);
+
+    const WebDavserver = new webdav.WebDAVServer({
+        port: 56789, //avoid default port, which might be already in use
+        hostname: '127.0.0.1' //localhost
+        //requireAuthentification: true,
+       // httpAuthentication: new webdav.HTTPBasicAuthentication(userManager, 'Default realm'),
+       // privilegeManager: privilegeManager
+    });
+    
+  //  var webdavOptions = new webdav.WebDAVServerOptions;
+
+ /*WebDavserver.rootFileSystem().addSubTree(WebDavserver.createExternalContext(), {
+    'folder1': {                                // /folder1
+        'file1.txt': webdav.ResourceType.File,  // /folder1/file1.txt
+        'file2.txt': webdav.ResourceType.File   // /folder1/file2.txt
+    },
+    'file0.txt': webdav.ResourceType.File       // /file0.txt
+})*/
+
+    
+    WebDavserver.afterRequest((arg, next) => {
+        console.log('>>', arg.request.method, arg.fullUri(), '>', arg.response.statusCode, arg.response.statusMessage, arg.response.body);
+        next();
+    })
+    
+    WebDavserver.start(httpServer => {
+        console.log('Server started with success on the port: ' + httpServer.address().port);
+        console.log('address: ' + httpServer.address().address);
+        console.log('family: ' + httpServer.address().family);
+        console.log('httpServer: ' + httpServer);
+        
+    });
+
+    
+    /*WebDavserver.setFileSystem('/physicalFolder', new webdav.PhysicalFileSystem('/my/path/to/the/folder'), (success) => {
+        WebDavserver.start(() => console.log('READY'));
+    })*/
+
     // Store time of start in cached localconfig to force reload of clients after server restart
     localConfig.startTime = Date.now();
     console.log(`Server started at ${localConfig.startTime}.`)
