32be5db96c429fe14842b2401d9d7bd507b1196c
hilderonny <ronny.hildebrandt@avorium.de>
2018-05-31 11:32:19 +0200

    Fixed tests.

--- api/menu.js
+++ api/menu.js
@@ -8,24 +8,6 @@ var co = require('../utils/constants');
 var configHelper = require('../utils/configHelper');
 var Db = require("../utils/db").Db;
 
-function extractAppsFromModules(modulenames) {
-    var mccopy = JSON.parse(JSON.stringify(mc));
-    var apps = {};
-    modulenames.forEach((modulename) => {
-        var mcmodule = mccopy.modules[modulename];
-        if (!mcmodule || !mcmodule.apps) return;
-        Object.keys(mcmodule.apps).forEach((apptitle) => {
-            var app = mcmodule.apps[apptitle];
-            if (!apps[apptitle]) {
-                apps[apptitle] = JSON.parse(JSON.stringify(app)); // Make copy instead of reference
-            } else {
-                Array.prototype.push.apply(apps[apptitle], app); // Push multiple items with prototype.push
-            }
-        });
-    });
-    return apps;
-}
-
 /**
  * Only the menu structure available for the current user is returned.
  * When the logged in user is an admin, 
@@ -58,16 +40,22 @@ router.get('/', auth(), async(req, res) => {
     var appsfromdatabase = await Db.getDynamicObjects(clientname, co.collections.apps.name);
     var apps = {};
     appsfromdatabase.forEach(a => apps[a.name] = { app: a, views: [] });
-    var viewsfromdatabase = (await Db.query(clientname, `
+    var viewsfromdatabase = modulenames.length > 0 ? (await Db.query(clientname, `
         SELECT DISTINCT a.name appname, v.*
         FROM apps a
         JOIN relations r ON r.datatype1name='apps' AND r.name1=a.name AND r.datatype2name='views' AND r.relationtypename='parentchild'
         JOIN views v ON r.name2=v.name
         JOIN users u ON 1=1
         LEFT JOIN permissions p ON p.usergroupname=u.usergroupname
-        WHERE u.name='${Db.replaceQuotes(req.user.name)}' AND (u.isadmin=true OR p.key=v.permission)
-        ;`)).rows;
+        WHERE u.name='${Db.replaceQuotes(req.user.name)}'
+        AND (u.isadmin=true OR p.key=v.permission)
+        AND v.modulename IN (${modulenames.map((k) => `'${Db.replaceQuotes(k)}'`).join(",")})
+        ;`)).rows : [];
     viewsfromdatabase.forEach(v => apps[v.appname].views.push(v));
+    // Delete empty apps
+    Object.keys(apps).forEach(k => {
+        if (apps[k].views.length < 1) delete apps[k];
+    });
     var result = {
         logourl: clientSettings && clientSettings.logourl ? clientSettings.logourl : 'css/logo_avorium_komplett.svg',
         apps: apps
