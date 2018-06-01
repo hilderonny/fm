a8bb126b3d5823a62e64983ae773535846a7a705
hilderonny <ronny.hildebrandt@avorium.de>
2018-06-01 09:07:23 +0200

    Updated display labels of references in detail cards. #AVTFM-158 work Entwicklung 1h

--- utils/updateonstart.js
+++ utils/updateonstart.js
@@ -8,7 +8,7 @@ var fs = require("fs");
 
 // 31.05.2018: AVTFM-158 - Erweiterung von Flächenarten
 async function convertareatypes(clientname) {
-    var namemap = {
+    var areanamemap = {
         "Wohnen und Aufenthalt": "nf1",
         "Büroarbeit": "nf2",
         "Produktion, Hand- und Maschinenarbeit, Experimente": "nf3",
@@ -21,10 +21,10 @@ async function convertareatypes(clientname) {
     };
     // Verweise suchen
     var relevantfields = (await Db.query(clientname, `SELECT * FROM datatypefields WHERE fieldtype='reference' AND reference='areatypes';`)).rows;
-    var names = Object.keys(namemap);
+    var names = Object.keys(areanamemap);
     for (var i = 0; i < names.length; i++) {
         var oldname = names[i];
-        var newname = namemap[oldname];
+        var newname = areanamemap[oldname];
         // Verknüpfungen
         await Db.query(clientname, `UPDATE relations SET name1='${newname}' WHERE datatype1name='areatypes' AND name1='${oldname}';`);
         await Db.query(clientname, `UPDATE relations SET name2='${newname}' WHERE datatype2name='areatypes' AND name2='${oldname}';`);
@@ -42,7 +42,36 @@ async function convertareatypes(clientname) {
     await ch.recalculateforupdateddatatype(clientname, "areatypes");
     // Titelfeld noch umbiegen
     await Db.query(clientname, "UPDATE datatypes SET titlefield='displayname' WHERE name='areatypes';");
-    // TODO: Kategorie-Namen sind noch alte Namen, noch nicht die Übersetzungs-Keys. Mal sehen, was davon falsch ist.
+}
+
+// 31.05.2018: AVTFM-158 - Erweiterung von Flächenarten
+async function convertareacategories(clientname) {
+    var categorynamemap = {
+        "Nutzungsfläche": "FMOBJECTS_CATEGORY_NUF",
+        "Technikfläche": "FMOBJECTS_CATEGORY_TF",
+        "Verkehrsfläche": "FMOBJECTS_CATEGORY_VF"
+    };
+    // Verweise suchen
+    var relevantfields = (await Db.query(clientname, `SELECT * FROM datatypefields WHERE fieldtype='reference' AND reference='areacategories';`)).rows;
+    var names = Object.keys(categorynamemap);
+    for (var i = 0; i < names.length; i++) {
+        var oldname = names[i];
+        var newname = categorynamemap[oldname];
+        // Verknüpfungen
+        await Db.query(clientname, `UPDATE relations SET name1='${newname}' WHERE datatype1name='areacategories' AND name1='${oldname}';`);
+        await Db.query(clientname, `UPDATE relations SET name2='${newname}' WHERE datatype2name='areacategories' AND name2='${oldname}';`);
+        // Verweise
+        for (var j = 0; j < relevantfields.length; j++) {
+            var field = relevantfields[j];
+            await Db.query(clientname, `UPDATE ${field.datatypename} SET ${field.name}='${newname}' WHERE ${field.name}='${oldname}';`);
+        }
+    }
+    // Alten Mist löschen, wenn möglich (wenn es die areacategories Tabelle nicht gibt: egal)
+    try {
+        await Db.query(clientname, `DELETE FROM areacategories WHERE name IN (${names.map(n => `'${n}'`).join(",")})`);
+    } catch(error) {}
+    // Formeln berechnen lassen
+    await ch.recalculateforupdateddatatype(clientname, "areacategories");
 }
 
 /**
@@ -54,7 +83,9 @@ module.exports = async() => {
     for (var i = 0; i < clients.length; i++) {
         var clientname = clients[i].name;
         convertareatypes(clientname);
+        convertareacategories(clientname);
     }
     convertareatypes(Db.PortalDatabaseName);
+    convertareacategories(Db.PortalDatabaseName);
     console.log("UPDATE FINISHED.");
 };
