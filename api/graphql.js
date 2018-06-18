10d79ffe442f49937ca0d88d8dc2ac4839c69448
Ronny Hildebrandt <ronny.hildebrandt@avorium.de>
2018-06-18 15:02:12 +0200

    The GraphQL API now suupports login and simple object requests. #AVTFM-197 work 3h

--- api/graphql.js
+++ api/graphql.js
@@ -1,179 +1,99 @@
 var router = require('express').Router();
 var auth = require("../middlewares/auth");
 var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+var graphql = require('graphql');
+var lh = require("../utils/loginHelper");
+var Db = require("../utils/db").Db;
 
-async function getSchema(user) {
-    console.log("GETSCHEMA", user);
-    return buildSchema(`
-        type Query {
-            hello: String
-        }
-    `);
+// Constructing schema without query language: https://graphql.org/graphql-js/constructing-types/
+function createLoginSchema() {
+    return new graphql.GraphQLSchema({
+        query: new graphql.GraphQLObjectType({
+            name: "Query",
+            description: "Use this query before each other call to login into the system and obtain an access token. After that use the token in any request as parameter \"?token=...\" for authentication.",
+            fields: {
+                login: { // Login endpoint
+                    type: graphql.GraphQLString,
+                    args: {
+                        username: { type: graphql.GraphQLString },
+                        password: { type: graphql.GraphQLString }
+                    },
+                    resolve: async(_, {username, password}) => {
+                        var loginresult = await lh.getToken(username, password);
+                        return loginresult ? loginresult.token : null;
+                    }
+                }
+            }
+        })
+    });
 }
 
-async function getRootValue(user) {
-    console.log("GETROOTVALUE", user);
-    var username = user ? user.name : "UNAMEDONE";
-    return  {
-        hello: () => {
-            return `Hello ${username}!`;
-        },
-    };
+/**
+ * Erstellt einen GraphQL Datentypen für einen internen Datentypen.
+ * Dabei haben momentan alle Felder den Typ String. Ist also derzeit 
+ * nur für das Auslesen sinnvoll.
+ */
+function extractDatatype(clientname, datatype) {
+    var objectfields = {};
+    Object.values(datatype.fields).forEach(f => {
+        objectfields[f.name] = { type: graphql.GraphQLString }
+    });
+    var objecttype = new graphql.GraphQLObjectType({
+        name: datatype.name,
+        fields: objectfields
+    });
+    return {
+        type: new graphql.GraphQLList(objecttype),
+        resolve: async() => {
+            return Db.getDynamicObjects(clientname, datatype.name);
+        }
+    }
+}
+
+/**
+ * Erstellt ein Schema für den angemeldeten Benutzer mit genau den Endpunkten, die für
+ * den Benutzer verfügbar sind.
+ */
+async function createUserSchema(user) {
+    var clientname = user.clientname;
+    var queryfields = {};
+    var datatypes = await Db.getdatatypes(clientname);
+    Object.values(datatypes).forEach(dt => {
+        if (dt.permissionkey && !user.permissions[dt.permissionkey]) return; // No access
+        queryfields[dt.name] = extractDatatype(clientname, dt);
+    });
+    return new graphql.GraphQLSchema({
+        query: new graphql.GraphQLObjectType({
+            name: "Query",
+            description: "This schema depends on the logged in user and provides only those endpoints available to him. When an user has read permissions for a datatype, he can query id. When the user also has write permissions, mutations for the datatype are provided too.",
+            fields: queryfields
+        })
+    });
+}
+
+/**
+ * Calculate schema for the currently logged in user.
+ * For unauthenticated users (without token) only the login API
+ * is available
+ */
+async function getSchema(user) {
+    return user ? createUserSchema(user) : createLoginSchema();
 }
 
 /**
  * Interactive endpoint GraphiQL
  */
-router.get('/', auth.try(), graphqlHTTP(async (req, res, graphQLParams) => ({
+router.get('/', auth.try(), graphqlHTTP(async (req) => ({
     schema: await getSchema(req.user),
-    rootValue: await getRootValue(req.user),
     graphiql: true
 })));
 
 /**
  * GraphQL API endpoint
  */
-router.post('/', auth.try(), graphqlHTTP(async (req, res, graphQLParams) => ({
+router.post('/', auth.try(), graphqlHTTP(async (req) => ({
     schema: await getSchema(req.user),
-    rootValue: await getRootValue(req.user),
     graphiql: false
 })));
 
-//  router.post('/', auth.try(), async (req, res) => {
-//     var schema = buildSchema(`
-//         type Query {
-//             hello: String
-//         }
-//     `);
-//     var root = {
-//         hello: () => {
-//             return 'Hello world!';
-//         },
-//     };
-//     var response = await graphql(schema, '{ hello }', root);
-//     res.send(response);
-// });
-
 module.exports = router;
-
-// // API for providing GraphQL API access
-// var graphqlHTTP = require('../middlewares/graphql');
-// var graphql = require('graphql');
-// var { buildSchema } = require('graphql');
-// var mc = require("../config/module-config.json");
-// var Db = require("../utils/db").Db;
-
-// var clientname = "737e7b80437b45e5b0def70b55d022df";
-
-// var portaldatatypes = {};
-// var clientdatatypes = {};
-// var alldatatypes = {};
-
-// Object.values(mc.modules).forEach(m => {
-//     if (m.portaldatatypes) {
-//         m.portaldatatypes.forEach(pdt => {
-//             portaldatatypes[pdt.name] = pdt;
-//             alldatatypes[pdt.name] = pdt;
-//         });
-//     }
-//     if (m.clientdatatypes) {
-//         m.clientdatatypes.forEach(cdt => {
-//             clientdatatypes[cdt.name] = cdt;
-//             alldatatypes[cdt.name] = cdt;
-//         });
-//     }
-// });
-
-// function createType(typename, fields) {
-//     var typedef = `type ${typename} {\n${fields.join("\n")}\n}\n`;
-//     return typedef;
-// }
-
-// // function createQueryType() {
-// //     //var fields = Object.keys(datatypes).map(dtn => `${dtn}: [${dtn}]\n`);
-// //     return createType("Query", fields);
-// // }
-
-// function createSchema() {
-//     var typedefs = [];
-//     Object.values(alldatatypes).forEach(dt => {
-//         var fields = [ "name: String!" ];
-//         if (dt.fields) dt.fields.forEach(f => {
-//             fields.push(`${f.name}: String`);
-//         });
-//         typedefs.push(createType(dt.name, fields));
-//     });
-//     typedefs.push(createType("Portal", Object.keys(portaldatatypes).map(pdtn => `${pdtn}: [${pdtn}]\n`)));
-//     typedefs.push(createType("Client", Object.keys(clientdatatypes).map(cdtn => `${cdtn}: [${cdtn}]\n`)));
-//     typedefs.push(createType("Query", [ "portal: Portal", "client(name: String): Client" ]));
-//     return typedefs.join("\n");
-// }
-
-// function createRoot() {
-//     var rootNode = {
-//         portal: () => {
-//         },
-//         client: (params) => {
-//             var clientNode = {};
-//             Object.values(clientdatatypes).forEach(dt => {
-//                 clientNode[dt.name] = () => {
-//                     return Db.getDynamicObjects(params.name, dt.name);
-//                 }
-//             });
-//             return clientNode;
-//         }
-//     };
-//     Object.values(portaldatatypes).forEach(dt => {
-//         rootNode.portal[dt.name] = () => {
-//             return Db.getDynamicObjects(Db.PortalDatabaseName, dt.name);
-//         }
-//     });
-//     return rootNode;
-// }
-
-// var queryType = new graphql.GraphQLObjectType({
-//     name: 'Query',
-//     fields: {
-//         portal: {
-//             type: graphql.GraphQLString
-//         },
-//         final: {
-//             type: graphql.GraphQLString
-//         }
-//     }
-// });
-
-// var schema = new graphql.GraphQLSchema({
-//     query: queryType
-// });
-
-// // var schema = buildSchema(createSchema());
-// var rootNode = createRoot();
-
-// var options = {
-//     schema: schema,
-//     rootValue: rootNode,
-//     graphiql: true,
-// };
-
-// var middleware = graphqlHTTP(options);
-
-// // options.schema und options.rootType lassen sich nachträglich ändern. So können dynamisch
-// // Schemas umdefiniert werden.
-// // Besser wäre es aber, in der eigenen Middleware Funktionen getSchema() und getRootType()
-// // zu verwenden, damit auch abhängig von der Anmeldung unterschiedliche Schemata zurück gegeben
-// // werden können
-
-// // console.log(options.schema);
-// setTimeout(() => {
-//     options.schema = buildSchema(`
-//     type Query {
-//         furz: String
-//         husten: String
-//     }
-//     `);
-//     // console.log(options.schema);
-// }, 10000);
-
-// module.exports = middleware;
