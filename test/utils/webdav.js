7963ccda135ec69a58ff1335e4b98ccdd8d0bdf2
ranafawzi <ranafawzi85@yahoo.com>
2018-06-28 09:20:54 +0200

    update  webdav uint test #AVTFM-176 6h

--- test/utils/webdav.js
+++ test/utils/webdav.js
@@ -15,7 +15,7 @@ var dh = require("../../utils/documentsHelper");
 
 
 
-describe('UTILS webdav', () => {
+describe.only('UTILS webdav', () => {
 
     var WebdavCleintConnection = (usernameInput, passwordInput) => {
         process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
@@ -27,6 +27,7 @@ describe('UTILS webdav', () => {
         });
         return conn;
     };
+
 
     before(async () => {
         await th.cleanDatabase();
@@ -45,7 +46,7 @@ describe('UTILS webdav', () => {
 
     //custom User Manager
     describe('getUserByName', () => {
-        xit('Function invocation made with non-existing username returns Error.BadAuthentication', async () => {  });
+        it('Function invocation made with non-existing username returns Error.BadAuthentication', async () => { });
         xit('Function invocation made with valid username returns deliver correct user data', async () => {
         });
     });
@@ -70,19 +71,42 @@ describe('UTILS webdav', () => {
                 });
             });
         });
+     
     });
 
     //customPrivilegeManager:   
     describe('_can', () => {
         xit('Function invocation made with non-existing clientName of the simpleUser retruns Permission denied');
         xit('Function invocation made with non-exist user returns Permission denied');
+        it.only('Function invocation made for valid user with non-privilege query returns Unauthorized ', async ()=>{
+           return new Promise(function(resolve, reject){
+               try{
+                   resolve (await th.removeReadPermission("client0", "client0_usergroup0", co.permissions.PERMISSION_OFFICE_DOCUMENT));
+                   var client = WebdavCleintConnection('client0_usergroup0_user0', 'test');
+                    client.readdir("/", (e,content)=>{            
+                    console.log(e,content);
+                    resolve();
+                });
+               }catch(e){ reject(e);}
+           
+
+               /**resolve(th.removeReadPermission("client0", "client0_usergroup0", co.permissions.PERMISSION_OFFICE_DOCUMENT));
+            }).then(function(resolve2, reject){
+                var client = WebdavCleintConnection('client0_usergroup0_user0', 'test');
+                client.readdir("/", (e,content)=>{            
+                    console.log(e,content);
+                    resolve2();
+                });**/
+            });
+        });
         xit('Function invocation made for valid read-only user with ‘canRead’ privilege query returns true');
-        xit('Function invocation made for valid read-only user with ‘canWrite’ privilege query returns false');
+        xit('Function invocation made for valid read-only user with ‘canWrite’ privilege query returns false', async ()=>{
+
+        });
         xit('Function invocation made for valid read-write user with ‘canWrite’ privilege query returns true');
         xit('Function invocation made for valid admin user with ‘canWrite’ privilege query returns true');
         xit('Function invocation made for valid non-authorized user with ‘canRead’ privilege query returns false');
-        xit('Function invocation made for valid non-authorized user with ‘canWrite’ privilege query returns false');
-
+        xit('Function invocation made for valid non-authorized user with ‘canWrite’ privilege query returns false');      
     });
     //WebDav Filesystem
     describe('type', () => {
@@ -246,6 +270,17 @@ describe('UTILS webdav', () => {
                 });
             });        
         });
+
+        it('Function invocation made with empty clientname and correct password returns unauthrized', async () =>{
+            return new Promise(function(resolve, reject){
+                var client = WebdavCleintConnection(null, 'test');
+                client.readdir("/", (e,content)=>{            
+                    console.log(e,content);
+                    resolve();
+                });
+            });
+
+        });   
 
         xit('Function invocation made as user without read rights returns initial login should fail');
     });
