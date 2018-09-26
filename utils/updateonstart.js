c3636eeaaa1b00d6d9c83a13f79029039e05f711
ranafawzi <ranafawzi85@yahoo.com>
2018-09-26 14:41:46 +0200

    Display a hierarchy list, table and a diagram for the flooring materials #AVTFM-244 work 13h

--- utils/updateonstart.js
+++ utils/updateonstart.js
@@ -7,19 +7,7 @@ var path = require("path");
 var fs = require("fs");
 
 
-async function deleteOldDatatypes(){    
-    var recordtypes=["communicationmediums", "communications","communicationtypes","fmobjects","fmobjecttypes"];
-    for(type=0; type<recordtypes.length; type++){
-        //first delete the recordtype from portal
-        await Db.deleteRecordType(Db.PortalDatabaseName, recordtypes[type]);
-        //Then delete it from all clients
-        var clientsresult = (await Db.query(Db.PortalDatabaseName, `SELECT * FROM clients;`)).rows;
-        for(i=0; i<clientsresult.length; i++ )
-        {
-            await Db.deleteRecordType(clientsresult[i].name, recordtypes[type]);
-        }        
-    }
-}
+
 
 /**
  * Update scripts which are run at system startup, when the localconfig flag "applyupdates" is set to true
